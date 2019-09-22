cd30bf49a573383754a3bd3cb3f7f275e557c109
David Johnston <dwtj@dwtj.me>
2019-09-21 21:05:25 -0700

    WIP: Try using `bazelbuild/bazel-federation`

---
+++ bazel_federation/myapp/HelloTest.java
@@ -0,0 +1,9 @@
+package myapp;
+
+import static myapp.Hello.hello;
+
+public class HelloTest {
+    public static void main(String[] args) {
+        hello();
+    }
+}
---
+++ bazel_federation/myapp/Main.java
@@ -0,0 +1,9 @@
+package myapp;
+
+import static myapp.Hello.hello;
+
+public class Main {
+    public static void main(String[] args) {
+        hello();
+    }
+}
